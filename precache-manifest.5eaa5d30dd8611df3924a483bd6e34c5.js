940a6a243365e6828adbcab7156943315fcc0f41
Peter Griffin <petergrieve33@gmail.com>
2018-11-17 00:59:26 -0500

    Updates

---
+++ precache-manifest.5eaa5d30dd8611df3924a483bd6e34c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "1f61c9dd654411d13034",
+    "url": "/clicky-game/static/js/main.1f61c9dd.chunk.js"
+  },
+  {
+    "revision": "ebdba860cf5c4ae1b86a",
+    "url": "/clicky-game/static/js/1.ebdba860.chunk.js"
+  },
+  {
+    "revision": "1f61c9dd654411d13034",
+    "url": "/clicky-game/static/css/main.4c515226.chunk.css"
+  },
+  {
+    "revision": "3633000c8bc312e53f27f1c6c0b24d47",
+    "url": "/clicky-game/index.html"
+  }
+];
